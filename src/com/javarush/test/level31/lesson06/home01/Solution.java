10750c01166b243891d3592578413e3f13ced2fe
ronaldo173 <my@mail>
2016-01-06 22:50:40 +0200

    changed add file to zip, it accepted!

--- src/com/javarush/test/level31/lesson06/home01/Solution.java
+++ src/com/javarush/test/level31/lesson06/home01/Solution.java
@@ -1,11 +1,13 @@
 package com.javarush.test.level31.lesson06.home01;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
-import java.util.Arrays;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
@@ -36,53 +38,61 @@ b.txt
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
-
+        if (args.length < 2) {
+            return;
+        }
         String fileName = args[0];
         String zipPath = args[1];
 
-        addFilesToExistingArchive(zipPath, fileName);
-    }
+        Path pathName = Paths.get(fileName);
+        Path pathToFile = Paths.get(zipPath);
 
-    private static void addFilesToExistingArchive(String zipPath, String fileName) throws IOException {
-        File zipFile = new File(zipPath);
-        //create temp file
-        File tempFile = new File(zipPath.substring(0, zipPath.length() - 4) + "_temp" + zipPath.substring(zipPath.length() - 4));
-        //rename our file to temp
-        zipFile.renameTo(tempFile);
-
-        FileInputStream fileInputStream = new FileInputStream(tempFile);
-        ZipInputStream zipInputStream = new ZipInputStream(fileInputStream);
-        ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(zipFile));
-
-        //add new file
-        File fileForAdd = new File(fileName);
-        ZipEntry entryOfNewFile = new ZipEntry("new/" + fileForAdd.getName());
-        zipOutputStream.putNextEntry(entryOfNewFile);
-        Files.copy(fileForAdd.toPath(), zipOutputStream);
-        //make split array of new path file to check it then
-        String[] splitNewFile = entryOfNewFile.getName().split("\\\\|/");
-//        System.out.println(entryOfNewFile.getName());
-
-        //add other files
-        ZipEntry zipEntry = null;
-        byte[] buff = new byte[4096];
-        while ((zipEntry = zipInputStream.getNextEntry()) != null) {
-
-            ZipEntry entryForAdd = new ZipEntry(zipEntry.getName());
-            String[] split = entryForAdd.getName().split("\\\\|/");
-            //check if added file contains in old archive, it will be instead of all
-            if (!Arrays.deepEquals(split, splitNewFile)) {
-                zipOutputStream.putNextEntry(entryForAdd);
-                int len;
-                while ((len = zipInputStream.read(buff)) > 0) {
-                    zipOutputStream.write(buff, 0, len);
-                }
-            }
+        if (!pathToFile.toFile().exists()) {
+            Files.createFile(pathToFile);
+        }
+        if (pathName.toFile().exists()) {
+            return;
         }
 
-        zipInputStream.close();
-        fileInputStream.close();
-        zipOutputStream.close();
-        tempFile.delete();
+        addToZip(pathToFile, pathName);
+
     }
+
+    public static void addToZip(Path zipFile, Path newFile) throws IOException {
+        Path tempZip = Files.createTempFile("temp", ".zip");
+        String tempRelativeWithoutNew = "new/" + zipFile.getParent().relativize(newFile);
+        Path relativePath = Paths.get(tempRelativeWithoutNew);
+
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(tempZip))) {
+            try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
+
+                ZipEntry zipEntry = zipInputStream.getNextEntry();
+                while (zipEntry != null) {
+                    Path pathOfFileInZip = Paths.get(zipEntry.getName());
+                    if (!pathOfFileInZip.equals(relativePath)) {
+                        zipOutputStream.putNextEntry(zipEntry);
+                        copyData(zipInputStream, zipOutputStream);
+                    }
+                    zipEntry = zipInputStream.getNextEntry();
+                }
+                zipInputStream.closeEntry();
+            }
+            FileInputStream fileInputStream = new FileInputStream(newFile.toFile());
+            ZipEntry zipEntry = new ZipEntry(relativePath.toString());
+            zipOutputStream.putNextEntry(zipEntry);
+            copyData(fileInputStream, zipOutputStream);
+            zipOutputStream.closeEntry();
+
+        }
+        Files.move(tempZip, zipFile, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private static void copyData(InputStream inputStream, OutputStream outputStream) throws IOException {
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, len);
+        }
+    }
+
 }
