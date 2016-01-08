bbbfdcced0ac14de421a923e99568ba8ab574ddd
Santer <ronaldo173@email.ua>
2016-01-09 01:53:52 +0200

    lvl 32 finished part22

--- src/com/javarush/test/level32/lesson15/big01/Controller.java
+++ src/com/javarush/test/level32/lesson15/big01/Controller.java
@@ -1,10 +1,10 @@
 package com.javarush.test.level32.lesson15.big01;
 
-import javax.swing.text.BadLocationException;
+import javax.swing.*;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
 
@@ -50,18 +50,6 @@ public class Controller {
         view.update();
     }
 
-    public void setPlainText(String text) {
-        resetDocument();
-        StringReader stringReader = new StringReader(text);
-        try {
-            HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
-            htmlEditorKit.read(stringReader, document, 0);
-
-        } catch (Exception e) {
-            ExceptionHandler.log(e);
-        }
-    }
-
     public String getPlainText() {
         StringWriter stringWriter = null;
         if (document != null) {
@@ -74,6 +62,18 @@ public class Controller {
             }
         }
         return stringWriter.toString();
+    }
+
+    public void setPlainText(String text) {
+        resetDocument();
+        StringReader stringReader = new StringReader(text);
+        try {
+            HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+            htmlEditorKit.read(stringReader, document, 0);
+
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
     }
 
     public void createNewDocument() {
@@ -89,22 +89,46 @@ public class Controller {
     public void saveDocument() {
     }
 
-    public void saveDocumentAs() {
-    }
-
     public void openDocument() {
 
     }
+
+    public void saveDocumentAs() {
+
+        view.selectHtmlTab();
+        JFileChooser jFileChooser = new JFileChooser();
+        jFileChooser.setFileFilter(new HTMLFileFilter());
+        jFileChooser.setDialogTitle("Save File");
+        int saveDialog = jFileChooser.showSaveDialog(view);
+
+        try {
+            if (saveDialog == JFileChooser.APPROVE_OPTION) {
+                currentFile = jFileChooser.getSelectedFile();
+                view.setTitle(currentFile.getName());
+
+                try (FileWriter fileWriter = new FileWriter(currentFile)) {
+                    HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+                    htmlEditorKit.write(fileWriter, document, 0, document.getLength());
+                }
+            }
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
 }
 /*
-Для открытия или сохранения файла мы будем использовать JFileChooser из библиотеки swing.
-Объекты этого типа поддерживают фильтры, унаследованные от FileFilter. Сейчас мы напишем
-свой фильт:
-21.1.	Создай публичный класс HTMLFileFilter унаследованный от FileFilter.
-21.2.	Мы хотим, чтобы окно выбора файла отображало только html/htm файлы или папки.
-Переопредели метод accept(File file), чтобы он возвращал true, если переданный файл
-директория или содержит в конце имени ".html" или ".htm" без учета регистра.
-21.3.	Чтобы в окне выбора файла в описании доступных типов файлов отображался текст
-"HTML и HTM файлы" переопредели соответствующим образом метод getDescription().
-
+Реализуем в контроллере метод для сохранения файла под новым именем saveDocumentAs().
+Реализация должна:
+22.1.	Переключать представление на html вкладку.
+22.2.	Создавать новый объект для выбора файла JFileChooser.
+22.3.	Устанавливать ему в качестве фильтра объект HTMLFileFilter.
+22.4.	Показывать диалоговое окно "Save File" для выбора файла.
+22.5.	Если пользователь подтвердит выбор файла:
+22.5.1.	Сохранять выбранный файл в поле currentFile.
+22.5.2.	Устанавливать имя файла в качестве заголовка окна представления.
+22.5.3.	Создавать FileWriter на базе currentFile.
+22.5.4.	Переписывать данные из документа document в объекта FileWriter-а аналогично тому,
+как мы это делали в методе getPlainText().
+22.6.	Метод не должен кидать исключения.
+Проверь работу пункта меню Сохранить как...
  */
