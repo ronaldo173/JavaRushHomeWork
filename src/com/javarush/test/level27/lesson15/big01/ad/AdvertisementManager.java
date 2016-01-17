e7283c39a4fe6c9806557c9de06dff7277db68d0
ronaldo173 <my@mail>
2016-01-17 22:22:59 +0200

    27big task7 finished

--- src/com/javarush/test/level27/lesson15/big01/ad/AdvertisementManager.java
+++ src/com/javarush/test/level27/lesson15/big01/ad/AdvertisementManager.java
@@ -4,4 +4,8 @@ package com.javarush.test.level27.lesson15.big01.ad;
  * Created by Santer on 17.01.2016.
  */
 public class AdvertisementManager {
+    private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
 }
+/*
+5. В AdvertisementManager создадим final поле-ссылку на экземпляр AdvertisementStorage и назовем ее storage
+ */
